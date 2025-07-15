c5534f35ecd240a7767c9d905e5b1dcf663bf051
FadiazArts <pelajaransharing@gmail.com>
2025-07-15 23:35:21 +0700

    Adding function for chech two number where arer same or not

--- Src/halo.cpp
+++ Src/halo.cpp
@@ -1,8 +1,19 @@
-include<stdio.h> 
+#include<stdio.h> 
+
+int isEquals(int a, int b);
 
 int main()
 {
 	printf("Halo dunia");
+	if (isEquals(5, 10/2))
+		printf("The two number is equals");
+	else
+		printf("The two number isn't equals");
 	return 1;
+}
+
+int isEquals(int a, int b)
+{
+	return ((a == b) ? (1) : (0));
 }
 	
